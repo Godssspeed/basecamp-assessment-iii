a01a777a9dc94fcce1642709da92fd043cc24e82
Godssspeed <33368773+Godssspeed@users.noreply.github.com>
2018-12-22 13:57:07 -0600

    Three components project = complete

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import SearchBar from './components/SearchBar/SearchBar.js';
+import BusinessList from './components/BusinessList/BusinessList.js';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList />
+      </div>
+    );
+  }
+}
+
+export default App;
